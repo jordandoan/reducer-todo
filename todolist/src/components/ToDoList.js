f05b05c217be6ccfc289978384b2154cf39f7136
Jordan <jordandoan@hotmail.com>
2019-09-09 16:54:05 -0700

    Fixed visual error when clearing completed tasks

--- todolist/src/components/ToDoList.js
+++ todolist/src/components/ToDoList.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
 
 const List = ({ list, dispatcher }) => {
   const handleClick = (event, task) => {
     dispatcher({type:'COMPLETE_TASK', payload: {...task, completed:!task.completed}});
-    event.target.parentNode.classList.toggle("completed");
+    // event.target.parentNode.classList.toggle("completed");
   }
+
+  useEffect(() => {
+    let tasks = document.querySelectorAll('.task');
+    tasks.forEach((task) => {
+      if (task.attributes.value.value == 'true') {
+        task.classList.add("completed");
+      } else {
+        task.classList.remove("completed");
+      }
+    })
+  }, [list])
   return (
     <div>
       {list.map(task => 
-        <div>
+        <div className='task' value={task.completed}>
           <p>{task.item}</p>
           <button onClick={(e) => handleClick(e, task)}>Complete Task</button>
         </div>
